6c5efce0e43af0855ac2d15edc7738b7cc2a139d
willtech84 <krasinskyekuroli@gmail.com>
2026-08-03 08:17:01 -0300

    Add AgendaScreen widget for agenda functionality

---
+++ lib/screens/home/lib/screens/designacoes/lib/screens/agenda/agenda_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AgendaScreen extends StatelessWidget {
+
+  const AgendaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Agenda"),
+      ),
+      body: const Center(
+        child: Text("Em desenvolvimento"),
+      ),
+    );
+  }
+}
