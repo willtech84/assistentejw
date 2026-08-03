a972b2da5922a280b8c8cb9f13c2a2e9346be690
willtech84 <krasinskyekuroli@gmail.com>
2026-08-03 08:12:33 -0300

    Add SplashScreen with navigation delay
    Implement a splash screen that navigates to home after 2 seconds.

---
+++ lib/lib/screens/splash/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => context.go("/home"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
