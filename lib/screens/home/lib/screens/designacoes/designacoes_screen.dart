07792c6b7490a6cc2656bb638242ca7f47aa4e6a
willtech84 <krasinskyekuroli@gmail.com>
2026-08-03 08:15:00 -0300

    Create DesignacoesScreen with basic layout

---
+++ lib/screens/home/lib/screens/designacoes/designacoes_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DesignacoesScreen extends StatelessWidget {
+
+  const DesignacoesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Designações"),
+      ),
+      body: const Center(
+        child: Text("Em desenvolvimento"),
+      ),
+    );
+  }
+}
