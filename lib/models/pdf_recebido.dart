19acaec7261bc1d8746ddc18a97a7a312c3a4b92
willtech84 <krasinskyekuroli@gmail.com>
2026-08-06 08:52:58 -0300

    Create PdfRecebido model class

---
+++ lib/models/pdf_recebido.dart
@@ -0,0 +1,11 @@
+// lib/models/pdf_recebido.dart
+
+class PdfRecebido {
+  final String nomeArquivo;
+  final String caminho;
+
+  const PdfRecebido({
+    required this.nomeArquivo,
+    required this.caminho,
+  });
+}
